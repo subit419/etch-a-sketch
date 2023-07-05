955f7daceb52e53d3c7215e17ed1b8d82c56159c
subit419 <12897494+subit419@users.noreply.github.com>
2023-07-05 10:53:57 -0400

    changed to use grid instead of flex

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,48 @@
-const divRows = 16;
-const divColumns = 16;
+const DEFAULT_GRID_SIZE = 16;
 const container = document.querySelector('.container');
 
-for (let row = 1; row <= divRows; row++) {
-    // Each Row has 16 columns of divs
-    var divRow = document.createElement("Div");
-    divRow.id = `row${row}`;
-    divRow.className = "row"
+// for (let row = 1; row <= divRows; row++) {
+//     // Each Row has 16 columns of divs
+//     var divRow = document.createElement("Div");
+//     divRow.id = `row${row}`;
+//     divRow.className = "row"
     
-    container.appendChild(divRow);
-    let currentRow = document.querySelector(`#row${row}`);
-    console.log(currentRow);
+//     container.appendChild(divRow);
+//     let currentRow = document.querySelector(`#row${row}`);
+//     console.log(currentRow);
 
-    for (let column = 1; column <= divColumns; column++) {
-        var divColumn = document.createElement("Div");
-        divColumn.id = `column${column}`;
-        divColumn.className = "column"
+//     for (let column = 1; column <= divColumns; column++) {
+//         var divColumn = document.createElement("Div");
+//         divColumn.id = `column${column}`;
+//         divColumn.className = "cell"
 
         
-        currentRow.appendChild(divColumn);
+//         currentRow.appendChild(divColumn);
+//     }
+// }
+function setupGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridCell = document.createElement('div');
+      gridCell.classList.add('cell');
+      gridCell.addEventListener('mouseover', colorCell)
+      container.appendChild(gridCell);
     }
+  }
+
+
+function colorCell(e) {
+    this.style.backgroundColor = "black";
+}
+
+
+var slide = document.getElementById('sizeSetting'),
+    sliderDiv = document.getElementById('sliderValue');
+
+slide.onchange
+
+window.onload = () => {
+    setupGrid(DEFAULT_GRID_SIZE);
 }
